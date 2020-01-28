33147e40a60e049e2aa074fe95d1a853ec2d325f
Volkan <mrvolkan163@gmail.com>
2020-01-28 11:18:00 +0100

    abc

--- Labb2/choice.py
+++ Labb2/choice.py
@@ -4,12 +4,7 @@ import json
 def readFile(filename, path="./"):
     fileLocation = path + filename
     lines = []
-    x= 0
-    users = {
-        "namn": " ",
-        "Efternamn": " ",
-        "Användarnamn": " ",
-    }
+    users = []
     try:
         with open(fileLocation, encoding="utf-8") as file_obj:
             for line in file_obj:
@@ -18,23 +13,29 @@ def readFile(filename, path="./"):
         print(ferr)
         return None
 
-    for line in lines:
-        print(line.rstrip("\n"))
+    # for line in lines:
+    #     print(line.rstrip("\n"))
 
-    [x.split(";")[0] for x in lines]
-    for x in users:
-        users["namn"] = [x]
-
-    [x.split(";")[1] for x in lines]
-    for x in users:
-        users["Efternamn"] = [x]
-
-    [x.split(";")[2] for x in lines]
-    for x in users:
-        users["Användarnamn"] = [x]
-        
     for x in lines:
-        newList = [
-            users
-        ]
-        print(newList)
+        info = x.split(";")
+        users.append({"namn": info[0],
+                    "Efternamn": info[1],
+                 "Användarnamn": info[2],
+                     "Mail": info[3]})
+    
+    for x in users:
+        print(x.rstrip("\n"))
+
+    # [x.split(";")[1] for x in lines]
+    # for x in users:
+    #     users["Efternamn"] = [x]
+
+    # [x.split(";")[2] for x in lines]
+    # for x in users:
+    #     users["Användarnamn"] = [x]
+
+    # for x in lines:
+    #     newList = [
+    #         users
+    #     ]
+    #     print(newList)
