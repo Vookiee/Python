aeb4e1e92067f687b826a069c80b651086a4a212
Volkan <Vookiee@users.noreply.github.com>
2020-02-04 19:23:01 +0100

    ny fil

---
+++ Labb3/program.py
@@ -0,0 +1,14 @@
+import requests
+
+def main ():
+    meny()
+
+
+def meny():
+    print("Välkommen till filmvärlden!")
+    print("1. Sök efter en film: ")
+    print("2. Historik")
+    print("3. Avsluta")
+    
+    
+main()    
