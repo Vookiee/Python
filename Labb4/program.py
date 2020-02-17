21f99afccd307957575d6ae9fd5d7dea448c258e
Volkan <Vookiee@users.noreply.github.com>
2020-02-17 16:39:23 +0100

    abc

--- Labb4/program.py
+++ Labb4/program.py
@@ -1,11 +1,21 @@
-from flash import Flask, render_template
-from flash import request
+from flask import Flask, render_template
+from flask import request
+import sqlite3
+import http.server
 
-app = Flash(__name__)
+app = Flask(__name__,template_folder='index')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-print("hello")
+# @app.route('/register', methods=['POST'])
+# def register():
+#     print(request.form.values)
+#     print(request.form.get('payment', 'Not set'))
+    
+    
+# app.run(host='0.0.0.0', port = 5000)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
