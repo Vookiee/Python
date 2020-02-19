73b9239846d7a4099ede5b5e7c5b7a6aa2827005
Volkan <Vookiee@users.noreply.github.com>
2020-02-19 16:32:31 +0100

    abc

--- Labb4/program.py
+++ Labb4/program.py
@@ -3,23 +3,39 @@ from flask import request
 import sqlite3
 import json
 
-app = Flask(__name__,template_folder='index')
+app = Flask(__name__)
 
-@app.route('/')
+@app.route('/template')
 def index():
     return render_template('index.html')
 
+@app.route('/', methods=['POST'])
+def register():
+    killer = [
+    request.form.get('fname'),
+    request.form.get('ename'),
+    request.form.get('address'),
+    request.form.get('zipcode'),
+    request.form.get('city'),
+    request.form.get('cellphone'),
+    request.form.get('email'),
+    request.form.get('paymenttype'),
+    request.form.get('offers'),
+    request.form.get('format'),
+    request.form.get('comment')]
 
-@app.route('/volkan', methods =['GET'])
-def signup(data= None):
-    hej = sqlite3.connect('signup.db')
-    volkan = hej.cursor()
-    volkan.execute('SELECT fornamn, id FROM users')
-    users = volkan.fetchall()
-    nousers = len(users)
-    return render_template('index.html', username = data, users = users, len = nousers)
+    f_name = killer[0]
+    e_name = killer[1]
+    address = killer[2]
+    postnmr = killer[3]
+    stad = killer[4]
+    telnmr = killer[5]
+    email = killer[6]
+    betala = killer[7]
+    offer = killer[8]
+    form = killer[9]
+    kommentar = killer[10]
+    return render_template('index.html', fname = f_name, ename =e_name, address=address, postnmr =postnmr, stad=stad, tele=telnmr, email=email, pay=betala, offer=offer, for_mat=form, kommentar=kommentar)
 
-@app.route('/', methods = ['POST'])
-def volkan():
-    print(request.form)
-    return { 'status' : f'Added user with username: {request.form.get("fornamn")}' }
+if __name__ == '__main__':
+    app.run()
